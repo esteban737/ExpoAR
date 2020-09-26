4582d539022681082043cffc10fa219753d92384
esteban737 <perdomo.steven.77@knights.ucf.edu>
2020-09-26 16:59:55 -0400

    Modify query scripts into services

---
+++ Assets/Scripts/EchoInitializer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EchoInitializer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Services services = new Services();
+        services.setCompany("GOOGLE").GetComponent<echoAR>().Init();
+    }
+}
---
+++ Assets/Scripts/Services.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class Services : MonoBehaviour
+{
+    [SerializeField]
+    GameObject echoPreFab = (GameObject) AssetDatabase.LoadAssetAtPath("Assets/echoAr/echoAr.prefab", typeof(GameObject));
+
+    // Start is called before the first frame update
+    public GameObject setCompany(string company)
+    {
+        GameObject echo = GameObject.Find("echoAR");
+        if (echo)
+        {
+            Vector3 pos = echo.transform.position;
+            Destroy(echo);
+            echo = Instantiate(echoPreFab, pos, Quaternion.identity);
+        }
+        else
+        {
+            echo = Instantiate(echoPreFab, Vector3.zero, Quaternion.identity);
+        }
+
+        echo.GetComponent<echoAR>().company = company;
+
+        return echo;
+    }
+
+}
