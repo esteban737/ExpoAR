341c9bf7350841cb08f9934f0e5729e4e272703a
Cris Sepulveda <csepulveda7@knights.ucf.edu>
2020-09-26 21:20:51 -0400

    Added Basic UI and plane detection

---
+++ Assets/Scripts/BtnManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BtnManager : MonoBehaviour
+{
+
+    private Button btn;
+    public GameObject merch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        btn = GetComponent<Button>();
+        btn.onClick.AddListener(SelectCompany);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SelectCompany()
+    {
+        DataHandler.Instance.merch = merch;
+    }
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+public class InputManager : MonoBehaviour
+{
+    // Reference to camera
+    [SerializeField] private Camera arCam;
+    [SerializeField] private ARRaycastManager _raycastManager;
+    
+    private List<ARRaycastHit> _hits = new List<ARRaycastHit>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Checks if there's been user input
+        if (Input.GetMouseButtonDown(0))
+        {
+            // When the user taps on the screen, move the ray to that position
+            Ray ray = arCam.ScreenPointToRay(Input.mousePosition);
+            if (_raycastManager.Raycast(ray, _hits))
+            {
+                // Give position & rotation of the hit
+                Pose pose = _hits[0].pose;
+                Instantiate(DataHandler.Instance.merch, pose.position, pose.rotation);
+            }
+        }
+    }
+}
