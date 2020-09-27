ba3d692a834fd3adef75a11fcece786de3992bdf
Cris Sepulveda <csepulveda7@knights.ucf.edu>
2020-09-27 04:31:54 -0400

    Added Start Menu & Help Icon

---
+++ Assets/Scripts/ShowHideUI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowHideUI : MonoBehaviour
+{
+    public GameObject panel;
+    public bool state;
+
+    public void SwitchShowHide()
+    {
+        state = !state;
+        panel.SetActive(state);
+    }
+}
